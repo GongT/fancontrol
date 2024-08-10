5ed59c281353c45861b16ff37a0322a63a803244
GongT <admin@gongt.me>
2024-08-11 00:02:15 +0800

    step

--- scripts/detect.sh
+++ scripts/detect.sh
@@ -20,21 +20,35 @@ for LINE in "${LINES[@]}"; do
 	TYPE="${PARTS[3]}"
 	# echo "$DEVNAME - $NAME - $VAL - $TYPE"
 
-	if [[ $NAME == "ST1000DM003-1ER162" ]]; then
+	if [[ $NAME == "ST1000DM003-1ER162" ]] || [[ $VAL == "ERR" ]]; then
 		continue
 	fi
 
-	if [[ $VAL -gt 45 ]]; then
+	if [[ 45 -le $VAL ]]; then
 		IS_HIGH=yes
 		echo "temperature of $DEVNAME($NAME) is high - $VAL$TYPE" >&2
 	fi
 done
 
+declare -i CURRENT MAX=210 MIN=60 FAN_INDEX=6
 detect_controller
-if [[ $IS_HIGH == yes ]]; then
-	echo "disk is hot, set fan speed to 210"
-	fan_level 6 210
-else
-	echo "no disk hot, set fan speed to 60"
-	fan_level 6 60
+
+CURRENT=$(get_fan_level $FAN_INDEX)
+if [[ $CURRENT -lt $MIN ]]; then
+	CURRENT=$MIN
 fi
+if [[ $CURRENT -gt $MAX ]]; then
+	CURRENT=$MAX
+fi
+
+if [[ $IS_HIGH == yes ]]; then
+	CURRENT=$((CURRENT + 10))
+	if [[ $CURRENT -gt $MAX ]]; then
+		CURRENT=$MAX
+	fi
+	echo "disk is hot, set fan speed to $CURRENT"
+else
+	echo "no disk hot, set fan speed to $MIN"
+	CURRENT=$MIN
+fi
+fan_level $FAN_INDEX "$CURRENT"
--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 export PATH=/bin:/usr/bin:/usr/local/bin:/usr/sbin
-DIR=
+HWM_DIR=
 
 function detect_controller() {
 	for I in /sys/class/hwmon/*/name; do
 		NAME=$(cat "$I" || true)
 		if [[ $NAME == "nct6795" ]]; then
-			DIR="$(dirname "$I")"
+			HWM_DIR="$(dirname "$I")"
 		fi
 	done
 
-	if ! [[ $DIR ]]; then
+	if ! [[ $HWM_DIR ]]; then
 		echo "PWM controller not found" >&2
 		exit 1
 	fi
@@ -20,6 +20,11 @@ function detect_controller() {
 function fan_level() {
 	local NUM=$1 VALUE=$2
 	echo " * pwm${NUM} --> $2" >&2
-	echo 1 >"$DIR/pwm${NUM}_enable"
-	echo "$VALUE" >"$DIR/pwm${NUM}"
+	echo 1 >"$HWM_DIR/pwm${NUM}_enable"
+	echo "$VALUE" >"$HWM_DIR/pwm${NUM}"
+}
+
+function get_fan_level() {
+	local NUM=$1
+	cat "$HWM_DIR/pwm${NUM}"
 }
